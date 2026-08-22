a453cf82e8c33f70a6ac53be1fff9624c0ff5bda
Mirjana Ristic <mima34.mg@gmail.com>
2026-08-22 15:08:06 +0200

    Add Domain entities (Product, Category) and repository interfaces

--- CleanShop.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CleanShop.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ CleanShop.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using CleanShop.Domain.Entities;
+
+namespace CleanShop.Domain.Interfaces;
+
+// Repository för produktkategorier
+public interface ICategoryRepository : IRepository<Category>
+{
+    // Kontrollerar om kategorin finns
+    Task<bool> ExistsAsync(int id);
+}
