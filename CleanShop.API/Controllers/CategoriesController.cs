4bc033ec75b084c69f1e1875e29366a8f5f0f7dc
Mirjana Ristic <mima34.mg@gmail.com>
2026-09-12 03:53:34 +0200

    Add API layer: ProductsController, CategoriesController, EF Core migrations, database connection

---
+++ CleanShop.API/Controllers/CategoriesController.cs
@@ -0,0 +1,51 @@
+﻿using CleanShop.Application.Categories.Commands.CreateCategory;
+using CleanShop.Application.Categories.Dtos;
+using CleanShop.Application.Categories.Queries.GetAllCategories;
+using CleanShop.Application.Categories.Queries.GetCategoryById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanShop.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CategoriesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<CategoryDto>>> GetAll()
+    {
+        // Skickar queryn vidare
+
+        var result = await _mediator.Send(new GetAllCategoriesQuery());
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CategoryDto>> GetById(int id)
+    {
+        var result = await _mediator.Send(new GetCategoryByIdQuery(id));
+
+        if (result is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> Create(CreateCategoryCommand command)
+    {
+        // Kör igång command
+
+        var id = await _mediator.Send(command);
+        return CreatedAtAction(nameof(GetById), new { id }, id);
+    }
+}
